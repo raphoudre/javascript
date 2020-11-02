c405ccd4d94c9c92bdc9de109d2a24f5256b1bb7
Raphoudre <raphael.ivol@hotmail.fr>
2020-11-02 13:01:21 +0100

    Ex 4 js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
-function check(){
-    var nbr;
-    nbr = Number(document.getElementById("myInput").value);
-    if(nbr%2 == 0)
-    {
-           alert("Nombre pair");
-    }
-    else
-    {
-           alert("Nombre impair");
-    }
+let nbr = prompt("entrer un chiffre entre 1 et 7");
+let day;
+switch (parseInt(nbr)) {
+    case 1:
+    day = "Lundi";
+    break;
+
+    case 2:
+    day = "Mardi";
+    break;
+
+    case 3:
+    day = "Mercredi";
+    break;
+
+    case 4:
+    day = "Jeudi";
+    break;
+
+    case 5:
+    day = "Vendredi";
+    break;
+    case 6:
+    day = "Samedi";
+    break;
+
+    case 7:
+    day = "Dimanche";
+    break;
 }
+alert(day)
